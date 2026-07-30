f0ce8e479f05d09cf73fa471c8b9f6a6c003c6f8
Dominick-codingal <dominickcodingal@gmail.com>
2026-07-30 13:32:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+name = "Dominic"
+age = 26
+weight = 65.7
+is_student = False
+
+print("name: ", name)
+print(type(name))
+
+print("age: ", age)
+print(type(age))
+
+print("is_student:",is_student)
+print(type(is_student))
+
+print("weight:",weight)
+print(type(weight))
+
